cbb9efff0f12f788a5300e6e079aed6fb7f992ff
静直樹 <naokishizuka@seijikaju-no-MacBook-Pro.local>
2015-10-24 14:30:06 +0900

    protoscontrollerを作成。ビューファイルの名前を変更。proto関係のルートを追加。

---
+++ app/controllers/protos_controller.rb
@@ -0,0 +1,4 @@
+class ProtosController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :users,only: [:edit,:update]
+  resources :protos,only: [:new]
 end
