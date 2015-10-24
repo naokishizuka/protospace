6b8e12726f755331c88c17343500130e096c6478
静直樹 <naokishizuka@seijikaju-no-MacBook-Pro.local>
2015-10-24 22:25:02 +0900

    不要なファイルを削除。カンマの後に空白を挿入。aタグをlink_toタグに変更。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :users,only: [:edit,:update]
-  resources :protos,only: [:new]
+  resources :users, only: [:edit,:update]
+  resources :protos, only: [:new]
 end
